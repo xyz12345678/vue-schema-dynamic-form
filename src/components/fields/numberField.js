1b5fded907bdd2bd0464728806a8d2ecfa79a4d7
xuefang.yang <xuefang.yang@msxf.com>
2018-11-08 11:40:01 +0800

    save

--- src/components/fields/numberField.js
+++ src/components/fields/numberField.js
@@ -7,13 +7,15 @@ export default {
             attrs: {
                 label: context.props.schemaField['label']
             },
-           
         },[
             createElement('el-input-number',{
                 attrs:context.props.schemaField['attrs'],
                 // 组件 props
+                min:context.props.schemaField['attrs'].min,
+                max:context.props.schemaField['attrs'].max,
                 props: {
                     value:context.props.value,
+                    
                 },
                 on:context.listeners
             })
